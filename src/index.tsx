eb8f20a94ad3df71028a158539d5a6adfc32c17e
mar1798 <m19a98r17@gmail.com>
2021-02-02 18:19:32 +0600

    init + 60% верстки

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {MuiThemeProvider} from "@material-ui/core";
+
+import './index.css';
+
+import App from './App';
+import {theme} from "./theme";
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+      <MuiThemeProvider theme={theme}>
+          <App />
+      </MuiThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
